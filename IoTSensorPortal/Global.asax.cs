1c55faf32384e2aa0f0ce8e10ed1c35ae4a4594f
ManchevCoding <manchevcoding@gmail.com>
2017-11-04 17:08:55 +0200

    Init++

--- IoTSensorPortal/Global.asax.cs
+++ IoTSensorPortal/Global.asax.cs
@@ -16,6 +16,9 @@ namespace IoTSensorPortal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
