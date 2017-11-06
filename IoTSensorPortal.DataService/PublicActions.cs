074460d5ee2216a6c5d5abee507cde57f1fe0dee
ManchevCoding <manchevcoding@gmail.com>
2017-11-06 15:57:26 +0200

    +classes: DB models, DataService classes

---
+++ IoTSensorPortal.DataService/PublicActions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTSensorPortal.DataService
+{
+    class PublicActions
+    {
+        //listing all sensors
+
+        //filter sensors type
+
+        //view sensor details by id partial view
+    }
+}
