051571623ac04c5a1f8158546090507345ad7b8b
Ditmar Vladislavov <didox222@gmail>
2017-11-07 15:24:33 +0200

    Admin log off fixed

--- IoTSensorPortal/Controllers/HomeController.cs
+++ IoTSensorPortal/Controllers/HomeController.cs
@@ -8,37 +8,37 @@ namespace IoTSensorPortal.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationUserManager userManager;
-        private readonly ApplicationDbContext applicationDbContext;
+        //private readonly ApplicationUserManager userManager;
+        //private readonly ApplicationDbContext applicationDbContext;
 
         public HomeController()
         { 
         }
-        public HomeController(ApplicationUserManager userManager, ApplicationDbContext applicationDbContext)
-        {
-            Guard.WhenArgument<ApplicationUserManager>(userManager, "userManager").IsNull();
-            this.userManager = userManager;
-            Guard.WhenArgument<ApplicationDbContext>(applicationDbContext, "applicationDbContext").IsNull();
-            this.applicationDbContext = applicationDbContext;
-        }
+        //public HomeController(ApplicationUserManager userManager, ApplicationDbContext applicationDbContext)
+        //{
+        //    Guard.WhenArgument<ApplicationUserManager>(userManager, "userManager").IsNull();
+        //    this.userManager = userManager;
+        //    Guard.WhenArgument<ApplicationDbContext>(applicationDbContext, "applicationDbContext").IsNull();
+        //    this.applicationDbContext = applicationDbContext;
+        //}
         public ActionResult Index()
         {
             return View();
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         public ActionResult ViewPublicSensors()
         {
             return View();
         }
 
         [Authorize]
-        public async Task<ActionResult>/*ActionResult*/ About()
+        public /*async Task<ActionResult>*/ActionResult About()
         {
-            this.applicationDbContext.Roles.Add(new IdentityRole() { Name = "Admin" });
-            await this.applicationDbContext.SaveChangesAsync();
-            var user = await this.userManager.FindByNameAsync(this.User.Identity.Name);
-            await this.userManager.AddToRoleAsync(user.Id, "Admin");
+            //this.applicationDbContext.Roles.Add(new IdentityRole() { Name = "Admin" });
+            //await this.applicationDbContext.SaveChangesAsync();
+            //var user = await this.userManager.FindByNameAsync(this.User.Identity.Name);
+            //await this.userManager.AddToRoleAsync(user.Id, "Admin");
 
             return View();
         }
