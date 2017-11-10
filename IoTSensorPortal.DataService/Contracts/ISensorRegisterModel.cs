2e1397ac7495a482ef228e0f24221aa38597753a
ManchevCoding <manchevcoding@gmail.com>
2017-11-10 20:45:13 +0200

    +ISensorRegistryModel implementation

--- IoTSensorPortal.DataService/Contracts/ISensorRegisterModel.cs
+++ IoTSensorPortal.DataService/Contracts/ISensorRegisterModel.cs
@@ -2,5 +2,16 @@
 {
     public interface ISensorRegisterModel
     {
+        string Url { get; set; } //sensor type user defined
+
+        string Name { get; set; } //can the user input a name or it comes from sensorInformation
+
+        int RefreshRate { get; set; } //user defined
+
+        int MinValue { get; set; } //user defined
+
+        int MaxValue { get; set; } //user defined
+
+        bool IsPublic { get; set; } //user defined
     }
 }
