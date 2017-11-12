61431d82b1d2ee99b1823f042f373b68a2f5f52a
CecoM <ceco_milchev@abv.bg>
2017-11-12 12:24:12 +0200

    Added items to the nav bar

---
+++ IoTSensorPortal/Scripts/jquery-dropdown.js
@@ -0,0 +1,11 @@
+﻿$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, 
+    hover: true, 
+    gutter: 0,
+    belowOrigin: false,
+    alignment: 'left',  
+    stopPropagation: false                                     
+}
+);
